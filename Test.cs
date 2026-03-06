ff852b69fa348a11535258de5d25aae658445c2a
Becelchak <88208993+Becelchak@users.noreply.github.com>
2026-03-06 10:37:42 +0500

    Work done

---
+++ Test.cs
@@ -0,0 +1,19 @@
+using NUnit.Framework;
+
+namespace VrKayak.Tests
+{
+    public class PlaceholderTests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass("Placeholder test passed");
+        }
+
+        [Test]
+        public void Test2()
+        {
+            Assert.That(2 + 2, Is.EqualTo(4));
+        }
+    }
+}

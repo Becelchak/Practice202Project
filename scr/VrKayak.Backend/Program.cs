759b2ff9a6babf96f501a613a502161a677d9d7b
Becelchak <88208993+Becelchak@users.noreply.github.com>
2026-03-31 17:49:27 +0500

    Update Program.cs

--- scr/VrKayak.Backend/Program.cs
+++ scr/VrKayak.Backend/Program.cs
@@ -4,6 +4,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
